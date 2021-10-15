f860b7b09b20a25f7e7afd335242587bdd8392ae
i7d3v3l0p3r <i7d3v3l0p3r@yandex.ru>
2021-10-15 14:23:51 +0000

    Refactoring

---
+++ src/interfaces.ts
@@ -0,0 +1,18 @@
+export type KEYCAP_SIZES = 1 | 1.25 | 1.5 | 1.75 | 2 | 2.25 | 6 | 6.25 | 6.5;
+
+export interface Point2D {
+    x: number;
+    y: number;
+}
+
+export interface KeyCapSize {
+    width: KEYCAP_SIZES;
+    height: KEYCAP_SIZES;
+}
+
+export interface AppSettings {
+    /** Size of 1u in px */
+    unitSize: number;
+    /** Keycap corner radius in relative to unitSize */
+    keyCapCornerRadius: number;
+}
